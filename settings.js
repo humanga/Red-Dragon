0d3f98ffe8674ff9fae4416705a3641c157d4335
humanga <114870515+humanga@users.noreply.github.com>
2022-10-12 19:31:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -56,7 +56,7 @@ type .alive to get alive msg😴
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
